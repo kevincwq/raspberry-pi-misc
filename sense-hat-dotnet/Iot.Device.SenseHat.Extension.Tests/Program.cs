a2fc112d5d156e58f3530d901fce055df483899c
Peter Chen <peter_chen@trimble.com>
2022-01-01 01:20:20 -0700

    add sensehat extension project and a test project

---
+++ sense-hat-dotnet/Iot.Device.SenseHat.Extension.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Iot.Device.SenseHat;
+using Iot.Device.SenseHat.Extension;
+using System.Drawing;
+
+using SenseHat sh = new SenseHat();
+var message = "+-*/!\"#$><0123456789.=)(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?,;:|@%[&_']\\~";
+
+while (!Console.KeyAvailable)
+{
+    sh.LedMatrix.ShowMessage(message, 100, Color.SkyBlue, Color.LightGray, LedMatrixExtension.Direction.Left);
+
+    sh.LedMatrix.ShowMessage(message, 100, Color.Red, Color.Black, LedMatrixExtension.Direction.Right);
+
+    sh.LedMatrix.ShowMessage(message, 100, Color.Green, Color.Black, LedMatrixExtension.Direction.Up);
+
+    sh.LedMatrix.ShowMessage(message, 100, Color.Green, Color.MediumPurple, LedMatrixExtension.Direction.Down);
+}
---
+++ sense-hat-dotnet/Iot.Device.SenseHat.Extension/LedMatrixExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace Iot.Device.SenseHat.Extension;
+
+public static class LedMatrixExtension
+{
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    public static void ShowMessage(this SenseHatLedMatrix ledMatrix, string message, int speedInMs = 90, Color? text_color = null, Color? back_color = null, Direction scrollDirection = Direction.Left)
+    {
+        Color t_color = text_color ?? Color.White;
+        Color b_color = back_color ?? Color.Black;
+        var msg = " " + message + " "; // padding
+        var isVertical = scrollDirection == Direction.Up || scrollDirection == Direction.Down;
+        var pixels = Font8x8.GetPixels(msg, t_color, b_color, isVertical);
+
+        // Shift right/bottom by 8 pixels per frame to scroll
+        var scroll_length = pixels.Length / 8 - 8;
+        for (int i = 0; i < scroll_length; i++)
+        {
+            var start = (scrollDirection == Direction.Left || scrollDirection == Direction.Up) ? i * 8 : pixels.Length - (i + 8) * 8;
+            ledMatrix.Write(pixels.Skip(start).Take(64).ToArray());
+            Thread.Sleep(speedInMs);
+        }
+    }
+
+    public static void ShowLetter(this SenseHatLedMatrix ledMatrix, char letter, Color? text_color = null, Color? back_color = null)
+    {
+        Color t_color = text_color ?? Color.White;
+        Color b_color = back_color ?? Color.Black;
+        var pixels = Font8x8.GetPixels(letter.ToString(), t_color, b_color);
+        ledMatrix.Write(pixels);
+    }
+
+    public static void Clear(this SenseHatLedMatrix ledMatrix)
+    {
+        ledMatrix.Fill(Color.Black);
+    }
+}
